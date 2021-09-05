6da850f72455d51ca02939a4c2da0f255b8298f7
cbellmont <carlos.bellmont@gmail.com>
2021-09-05 20:49:36 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        launchProcessAndWaiting();
+        launchProcessAndStopping();
+        launchProcessAndRead();
+    }
+
+    private static void launchProcessAndWaiting() {
+        String path = "C:\\WINDOWS\\system32\\notepad.exe";
+        String[] processToOpen = new String[1];
+        processToOpen[0] = path;
+        ProcessBuilder pb = new ProcessBuilder(processToOpen);
+
+        try {
+            // Creamos un proceso y lo empezamos
+            Process process = pb.start();
+            // Nos quedamos esperando a que termine.
+            int status = process.waitFor();
+            System.out.println("El resultado de la ejecución ha sido " + status);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void launchProcessAndStopping() {
+        String path = "C:\\WINDOWS\\system32\\notepad.exe";
+        String[] processToOpen = new String[1];
+        processToOpen[0] = path;
+        ProcessBuilder pb = new ProcessBuilder(processToOpen);
+
+        try {
+            // Creamos un proceso y lo empezamos
+            Process process = pb.start();
+            Thread.sleep(2000);
+            process.destroy();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void launchProcessAndRead() {
+        try {
+            ProcessBuilder builder = new ProcessBuilder(
+                    "cmd.exe", "/c", "cd \"C:\\\" && dir");
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while (true) {
+                line = r.readLine();
+                if (line == null) { break; }
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
